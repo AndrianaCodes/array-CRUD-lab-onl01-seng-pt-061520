cbcb8994ba7003bfc071fdbda4761a07ad2c9663
Andriana Garrido <andriana.amg@gmail.com>
2020-06-22 23:46:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,23 +3,24 @@ def create_an_empty_array
 end
 
 def create_an_array
-  an_array = ["eyeshadow", "mascara", "lipstick", "foundation"]
+  makeup_array = ["eyeshadow", "mascara", "lipstick", "foundation"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  makeup_array << "blush"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  makeup_array.unshift("dipbrow pomade")
 end
 
 def remove_element_from_end_of_array(array)
+  makeup_array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  makeup_array.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
