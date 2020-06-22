7df6f9ad0cde49afb53b63dae0af14d04971ed76
Andriana Garrido <andriana.amg@gmail.com>
2020-06-22 23:42:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  an_array = ["eyeshadow", "mascara", "lipstick"]
+  an_array = ["eyeshadow", "mascara", "lipstick", "foundation"]
 end
 
 def add_element_to_end_of_array(array, element)
